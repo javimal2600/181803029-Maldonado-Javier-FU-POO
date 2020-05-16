aac5d51455fa8d707d73851cd36eacb769e1e517
Javier Maldonado García <javimal26@gmail.com>
2020-05-16 09:17:29 -0500

    Mi primer Repositorio

---
+++ Hola_Mundo/src/main/java/Hola_mundo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Javier García
+ */
+import java.util.*;
+import java.util.Scanner;
+
+public class Hola_mundo {
+   
+
+public static void main(String[] args){
+    System.out.println("Hola mundo");
+    }
+
+}
