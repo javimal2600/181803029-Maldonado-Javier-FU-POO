8e49868b888102b6669501eefde9df0d1f9ba926
javimal2600 <65452640+javimal2600@users.noreply.github.com>
2020-06-02 17:27:06 -0500

    sistema de control

--- sistemaControl/src/main/java/Tarjetas.java
+++ sistemaControl/src/main/java/Tarjetas.java
@@ -9,6 +9,7 @@
  * @author Javier García
  */
 public class Tarjetas {
+
     int noCuenta;
     int nip;
     int cv;
@@ -18,46 +19,72 @@ public class Tarjetas {
     int anio;
     double montoDisponible;
     double montoApartado;
-    
+
     //constructor vacio
-    public Tarjetas(){}
-    //constructor con parametros
-    public Tarjetas(int noCuenta,int nip,int cv,String nombre,int dia, int mes, int anio,double montoDisponible,double montoApartado){
-        this.noCuenta=noCuenta;
-        this.nip=nip;
-        this.cv=cv;
-        this.nombre=nombre;
-        this.dia=dia;
-        this.mes=mes;
-        this.anio=anio;
-        this.montoDisponible=montoDisponible;
-        this.montoApartado=montoApartado;
-        
+    public Tarjetas() {
     }
-   
-    //
-    public void verificarCuenta(int noCuenta,int nip){
-        if(noCuenta==123123 && nip==1133){
-            System.out.println("Eres la cuenta de Eduardo Diaz Flores");
-            Tarjetas eduardo=new Tarjetas(123123,1133,543,"Eduardo Diaz Flores",31,12,2032,45987.22,550);
-            System.out.print(eduardo.toString());
-        }else if(noCuenta==456789 && nip== 3367){
-            System.out.println("Eres la cuenta de Kevin Mesa Gonzalez");
-            Tarjetas kevin=new Tarjetas(456789,3367,544,"Kevin Mesa Gonzalez",31,12,2032,360,5322.23);
-            System.out.print(kevin.toString());
-        }else{
-            System.out.println("No estas registrados");
+
+    //constructor con parametros
+    public Tarjetas(int noCuenta, int nip, int cv, String nombre, int dia, int mes, int anio, double montoDisponible, double montoApartado) {
+        this.noCuenta = noCuenta;
+        this.nip = nip;
+        this.cv = cv;
+        this.nombre = nombre;
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+        this.montoDisponible = montoDisponible;
+        this.montoApartado = montoApartado;
+    }
+
+    //1 public no le haremos caso
+    //2 que tipo se va a regresar en este caso(Tarjetas)
+    //3 Es el nombre
+    //4 los valores que recibe el metodo
+    public Tarjetas verificarCuenta(int noCuenta, int nip) {
+        if (noCuenta == 123123 && nip == 1133) {
+
+            return new Tarjetas(123123, 1133, 543, "Eduardo Diaz Flores", 31, 12, 2032, 45987.22, 550);
+
+        } else if (noCuenta == 456789 && nip == 3367) {
+
+            return new Tarjetas(456789, 3367, 544, "Kevin Mesa Gonzalez", 31, 12, 2032, 360, 5322.23);
+
+        } else {
+            return new Tarjetas();
         }
     }
-    
-     public double total (){
-        return this.montoDisponible+this.montoApartado;
+
+    public double total() {
+        return this.montoDisponible + this.montoApartado;
     }
 
+    //metodo para un deposito
+    public double deposito(double deposito) {
+        montoDisponible += deposito;
+        return montoDisponible;
+    }
+    
+    //metodo para hacer un retiro
+    public double retiro(double retiro) {
+        if (retiro <= montoDisponible) {
+            montoDisponible -= retiro;
+            System.out.println("¡Retiro realizado con exito! \n");
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+        return montoDisponible;
+    }
+
+    //metodo para eliminar el apartado
+    public void apartado() {
+        montoApartado = 0;
+    }
+
+    //metodo para mostrar los datos de la cuenta
     @Override
     public String toString() {
-        return  "\n No.Cuenta:" + noCuenta + " \n NIP: " + nip + "\n CV:" + cv + "\n Nombre: " + nombre + "\n Fecha Vencimiento: " + dia + "/" + mes + "/" + anio + "\n montoDisponible:" + montoDisponible + "\n montoApartado: " + montoApartado+"\n Total:"+this.total();
+        return "\n No.Cuenta:" + noCuenta + " \n NIP:" + nip + "\n CV:" + cv + "\n Nombre: " + nombre + "\n Fecha Vencimiento: " + dia + "/" + mes + "/" + anio + "\n montoDisponible:" + montoDisponible + "\n montoApartado: " + montoApartado + "\n Total:" + this.total();
     }
-    
-}   
 
+}
--- sistemaControl/src/main/java/tarjetasMain.java
+++ sistemaControl/src/main/java/tarjetasMain.java
@@ -13,16 +13,56 @@ import java.util.Scanner;
 public class tarjetasMain {
     public static void main(String[] args){
         Scanner leer = new Scanner(System.in);
-        Tarjetas tarjeta=new Tarjetas();
-      
+        //declarando al programa que usare un objeto de tipo Tarjetas
+        Tarjetas tarjeta;
+
         int noCuenta, nip;
         System.out.println("Ingresa tu numero de cuenta:");
-        noCuenta=leer.nextInt();
+        noCuenta = leer.nextInt();
         System.out.println("Ingresa tu NIP");
-        nip=leer.nextInt();
-        tarjeta.verificarCuenta(noCuenta, nip);
-        System.out.println("\n"+tarjeta.nombre);
-        
-        
+        nip = leer.nextInt();
+        //al mismo tiempo que estoy instanciando llamo el metodo verificarCuenta
+        tarjeta = new Tarjetas().verificarCuenta(noCuenta, nip);
+        //System.out.println(tarjeta.toString());
+
+        System.out.println("\n");
+
+        System.out.println("Tu nombre es: " + tarjeta.nombre);
+        //menu
+        int opc;
+        do {
+            System.out.println("1) Hacer un deposito");
+            System.out.println("2) Hacer un retiro");
+            System.out.println("3) Eliminar apartado");
+            System.out.println("4) Imprimir datos de la cuenta");
+            System.out.println("5) Salir");
+            System.out.println("Escoge una opcion:");
+            opc = leer.nextInt();
+            System.out.println("\n");
+            switch (opc) {
+                case 1:
+                    System.out.println("Introduzca la cantidad a depositar:");
+                    double deposito = leer.nextDouble();
+                    tarjeta.deposito(deposito);
+                    System.out.println("¡Deposito realizado con exito! \n");
+                    break;
+                case 2:
+                    System.out.println("Introduzca la cantida a retirar:");
+                    double retiro = leer.nextDouble();
+                    tarjeta.retiro(retiro);
+                    break;
+                case 3:
+                    System.out.println("Eliminando Apartado... \n");
+                    tarjeta.apartado();
+                    System.out.println("!Apartado eliminado con exito¡ \n");
+                    break;
+                case 4:
+                    System.out.println(tarjeta.toString());
+                    System.out.println("\n");
+                    break;
+            }
+        } while (opc != 5);
+        System.out.println("¡Gracias vuelva pronto!");
     }
 }
+
